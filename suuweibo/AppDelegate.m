0fc38ee0d11a54063117561f28808069eb65c813
suusatoshigi <suusatoshigi@gmail.com>
2015-05-02 12:35:36 +0800

    this is the basement ui for test

--- suuweibo/AppDelegate.m
+++ suuweibo/AppDelegate.m
@@ -23,6 +23,10 @@
     [super dealloc];
 }
 
+- (void)_initSinaWeibo {
+    
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds]autorelease]
