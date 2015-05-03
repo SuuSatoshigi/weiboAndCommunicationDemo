88c812b245631f1af6e8480ab177c22c07691844
XuWeihao <jmepxwh@gmail.com>
2015-05-03 16:12:58 +0800

    iphone real test problem done

--- suuweibo/AppDelegate.m
+++ suuweibo/AppDelegate.m
@@ -11,9 +11,10 @@
 #import "DDMenuController.h"
 #import "LeftViewController.h"
 #import "RightViewController.h"
+#import "WeiboSDK.h"
 
-
-@interface AppDelegate ()
+#import "CONSTS.h"ß
+@interface AppDelegate ()<WeiboSDKDelegate>
 
 @end
 
@@ -24,6 +25,8 @@
 }
 
 - (void)_initSinaWeibo {
+    [WeiboSDK enableDebugMode:YES];
+    [WeiboSDK registerApp:kAppKey];
     
 }
 
@@ -31,6 +34,7 @@
     // Override point for customization after application launch.
     self.window = [[[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds]autorelease]
 ;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     MainViewController *mainCtrl = [[MainViewController alloc] init];
     
@@ -46,6 +50,9 @@
     
     [self.window makeKeyAndVisible];
    
+    
+
+    
     return YES;
 }
 
@@ -69,6 +76,71 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)didReceiveWeiboResponse:(WBBaseResponse *)response
+{
+    if ([response isKindOfClass:WBSendMessageToWeiboResponse.class])
+    {
+        NSString *title = NSLocalizedString(@"发送结果", nil);
+        NSString *message = [NSString stringWithFormat:@"%@: %d\n%@: %@\n%@: %@", NSLocalizedString(@"响应状态", nil), (int)response.statusCode, NSLocalizedString(@"响应UserInfo数据", nil), response.userInfo, NSLocalizedString(@"原请求UserInfo数据", nil),response.requestUserInfo];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                        message:message
+                                                       delegate:nil
+                                              cancelButtonTitle:NSLocalizedString(@"确定", nil)
+                                              otherButtonTitles:nil];
+        WBSendMessageToWeiboResponse* sendMessageToWeiboResponse = (WBSendMessageToWeiboResponse*)response;
+        NSString* accessToken = [sendMessageToWeiboResponse.authResponse accessToken];
+        if (accessToken)
+        {
+            self.wbtoken = accessToken;
+        }
+        NSString* userID = [sendMessageToWeiboResponse.authResponse userID];
+        if (userID) {
+            self.wbCurrentUserID = userID;
+        }
+        [alert show];
+        [alert release];
+    }
+    else if ([response isKindOfClass:WBAuthorizeResponse.class])
+    {
+        NSString *title = NSLocalizedString(@"认证结果", nil);
+        NSString *message = [NSString stringWithFormat:@"%@: %d\nresponse.userId: %@\nresponse.accessToken: %@\n%@: %@\n%@: %@", NSLocalizedString(@"响应状态", nil), (int)response.statusCode,[(WBAuthorizeResponse *)response userID], [(WBAuthorizeResponse *)response accessToken],  NSLocalizedString(@"响应UserInfo数据", nil), response.userInfo, NSLocalizedString(@"原请求UserInfo数据", nil), response.requestUserInfo];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                        message:message
+                                                       delegate:nil
+                                              cancelButtonTitle:NSLocalizedString(@"确定", nil)
+                                              otherButtonTitles:nil];
+        
+        self.wbtoken = [(WBAuthorizeResponse *)response accessToken];
+        self.wbCurrentUserID = [(WBAuthorizeResponse *)response userID];
+        [alert show];
+        [alert release];
+    }
+    else if ([response isKindOfClass:WBPaymentResponse.class])
+    {
+        NSString *title = NSLocalizedString(@"支付结果", nil);
+        NSString *message = [NSString stringWithFormat:@"%@: %d\nresponse.payStatusCode: %@\nresponse.payStatusMessage: %@\n%@: %@\n%@: %@", NSLocalizedString(@"响应状态", nil), (int)response.statusCode,[(WBPaymentResponse *)response payStatusCode], [(WBPaymentResponse *)response payStatusMessage], NSLocalizedString(@"响应UserInfo数据", nil),response.userInfo, NSLocalizedString(@"原请求UserInfo数据", nil), response.requestUserInfo];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                        message:message
+                                                       delegate:nil
+                                              cancelButtonTitle:NSLocalizedString(@"确定", nil)
+                                              otherButtonTitles:nil];
+        [alert show];
+        [alert release];
+    }
+}
+
+
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    return [WeiboSDK handleOpenURL:url delegate:self];
+}
+
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    return [WeiboSDK handleOpenURL:url delegate:self ];
 }
 
 @end
---
+++ suuweibo/CONSTS.h
@@ -0,0 +1,16 @@
+//
+//  CONSTS.h
+//  suuweibo
+//
+//  Created by XuWeihao on 5/3/15.
+//  Copyright (c) 2015 weihaoxu. All rights reserved.
+//
+
+#ifndef suuweibo_CONSTS_h
+#define suuweibo_CONSTS_h
+
+
+#define kAppKey         @"1589467727"
+#define kRedirectURI    @"http://www.sina.com"
+
+#endif
